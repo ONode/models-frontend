d0acf052f1d2a5adbac0ae928be558930ed10042
CameronS29 <c.sato.dev@gmail.com>
2022-03-08 09:36:33 -0500

    enter page updated.

--- src/pages/enter/index.js
+++ src/pages/enter/index.js
@@ -1,74 +1,76 @@
-import React, { useState, useEffect } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
-import Modal from '../../components/modal/popup'
+import Modal from "../../components/modal/popup";
 
-import { getAccount, getUser } from '@selectors/user.selectors'
+import { getAccount, getUser } from "@selectors/user.selectors";
 
-import userActions from '@actions/user.actions'
-import styles from './styles.module.scss'
+import userActions from "@actions/user.actions";
+import styles from "./styles.module.scss";
 
 function getWindowDimensions() {
-  const { innerWidth: width, innerHeight: height } = window
+  const { innerWidth: width, innerHeight: height } = window;
   return {
     width,
     height,
-  }
+  };
 }
 
 function useWindowDimensions() {
-  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions())
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions()
+  );
 
   useEffect(() => {
     function handleResize() {
-      setWindowDimensions(getWindowDimensions())
+      setWindowDimensions(getWindowDimensions());
     }
 
-    window.addEventListener('resize', handleResize)
-    return () => window.removeEventListener('resize', handleResize)
-  }, [])
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
-  return windowDimensions
+  return windowDimensions;
 }
 
 function Home(props) {
-  const dispatch = useDispatch()
-  const user = useSelector(getUser)
-  const account = useSelector(getAccount)
+  const dispatch = useDispatch();
+  const user = useSelector(getUser);
+  const account = useSelector(getAccount);
   if (!user) {
-    dispatch(userActions.checkStorageAuth())
+    dispatch(userActions.checkStorageAuth());
   }
 
-  const [comingModalOpen, setComingModalOpen] = useState(false)
-  const [switchModalOpen, setSwitchModalOpen] = useState(false)
-  const [open, setOpen] = useState(false)
-  const [open1, setOpen1] = useState(false)
-  const [whitelisted, setWhitelisted] = useState(false)
-  const screenWidth = useWindowDimensions().width
-  const [isMobile, setIsMobile] = useState(false)
+  const [comingModalOpen, setComingModalOpen] = useState(false);
+  const [switchModalOpen, setSwitchModalOpen] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [open1, setOpen1] = useState(false);
+  const [whitelisted, setWhitelisted] = useState(false);
+  const screenWidth = useWindowDimensions().width;
+  const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
-    screenWidth > 376 ? setIsMobile(false) : setIsMobile(true)
-  }, [screenWidth])
+    screenWidth > 376 ? setIsMobile(false) : setIsMobile(true);
+  }, [screenWidth]);
 
   const handleClose1 = () => {
-    setOpen1(false)
-  }
+    setOpen1(false);
+  };
 
   const handleClose = () => {
-    setOpen(false)
-  }
+    setOpen(false);
+  };
 
   useEffect(() => {
     const fetchWhiteListed = async () => {
-      const resp = await userActions.checkWhitelisted(account)
-      if (resp !== whitelisted) setWhitelisted(resp)
-    }
+      const resp = await userActions.checkWhitelisted(account);
+      if (resp !== whitelisted) setWhitelisted(resp);
+    };
 
     if (account) {
-      fetchWhiteListed()
+      fetchWhiteListed();
     }
-  }, [account])
+  }, [account]);
 
   return (
     <div className={styles.wrapper}>
@@ -79,18 +81,28 @@ function Home(props) {
             <a href="/global">
               <img
                 src="/images/homepage/right_arrow.png"
-                className={[styles.arrowImage, "z-50 w-28 animate-horizonbounce"].join(' ')}
+                className={[
+                  styles.arrowImage,
+                  "z-50 w-28 animate-horizonbounce",
+                ].join(" ")}
                 alt=""
               />
             </a>
           </div>
 
-          <div className="relative productimg mt-10 md:mt-0" >
-            <img src="/images/product2.png" className="w-full enterimg" alt="" />
+          <div className="relative productimg mt-10 md:mt-0">
+            <img
+              src="/images/product2.png"
+              className="w-full enterimg"
+              alt=""
+            />
             <a href="https://runway.digitalax.xyz">
               <img
                 src="/images/homepage/right_arrow.png"
-                className={[styles.arrowImage, "z-50 w-28 animate-horizonbounce"].join(' ')}
+                className={[
+                  styles.arrowImage,
+                  "z-50 w-28 animate-horizonbounce",
+                ].join(" ")}
                 alt=""
               />
             </a>
@@ -102,58 +114,137 @@ function Home(props) {
           <div className={styles.rect2}></div>
 
           <div className="relative productimg mt-10 md:mt-0">
-            <img src="/images/product3.png" className="md:w-full enterimg" alt="" />
+            <img
+              src="/images/product3.png"
+              className="md:w-full enterimg"
+              alt=""
+            />
             <a href="/openelementals">
               <img
                 src="/images/homepage/right_arrow.png"
-                className={[styles.arrowImage, "z-50 w-28 animate-horizonbounce"].join(' ')}
+                className={[
+                  styles.arrowImage,
+                  "z-50 w-28 animate-horizonbounce",
+                ].join(" ")}
                 alt=""
               />
             </a>
           </div>
 
           <div className="relative productimg mt-10 md:mt-0">
-            <img src="/images/product4.png" className="w-full enterimg" alt="" />
+            <img
+              src="/images/product4.png"
+              className="w-full enterimg"
+              alt=""
+            />
             <a href="/avatarlibraries">
               <img
                 src="/images/homepage/right_arrow.png"
-                className={[styles.arrowImage, "z-50 w-28 animate-horizonbounce"].join(' ')}
+                className={[
+                  styles.arrowImage,
+                  "z-50 w-28 animate-horizonbounce",
+                ].join(" ")}
                 alt=""
               />
             </a>
           </div>
-   
         </div>
 
         <div className="relative flex flex-col justify-center items-center md:flex md:flex-row md:justify-between md:items-end w-full mt-16">
           <div className="relative productimg mt-10 md:mt-0">
             <img src="/images/product5.png" className="w-full" alt="" />
-            <a href="/minting"  target='_blank'>
+            <a href="/minting" target="_blank">
               <img
                 src="/images/homepage/right_arrow.png"
-                className={[styles.arrowImage, "z-50 w-28 animate-horizonbounce"].join(' ')}
+                className={[
+                  styles.arrowImage,
+                  "z-50 w-28 animate-horizonbounce",
+                ].join(" ")}
                 alt=""
               />
             </a>
           </div>
-          
+
           <div className="relative productimg mt-10 md:mt-0">
-            <img src="/images/product6.png" className="w-full enterimg" alt="" />
+            <img
+              src="/images/product6.png"
+              className="w-full enterimg"
+              alt=""
+            />
             <a href="https://realmrunway.xyz">
               <img
                 src="/images/homepage/right_arrow.png"
-                className={[styles.arrowImage, "z-50 w-28 animate-horizonbounce"].join(' ')}
+                className={[
+                  styles.arrowImage,
+                  "z-50 w-28 animate-horizonbounce",
+                ].join(" ")}
                 alt=""
               />
             </a>
           </div>
         </div>
 
-        <Modal open={comingModalOpen} handleClose={() => setComingModalOpen(false)}>
-          <p className="text-gray-50 font-normal text-base font-inter text-center">Coming Soon!</p>
+        <div className="relative flex flex-col justify-center items-center md:flex md:flex-row md:justify-between md:items-end w-full mt-16">
+          <div className={styles.rect2}></div>
+
+          <div className="relative productimg mt-10 md:mt-0">
+            <img
+              src="/images/product7.png"
+              className="md:w-full enterimg"
+              alt=""
+            />
+            <a
+              href="https://soundcloud.com/modelsalamode"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                src="/images/homepage/right_arrow.png"
+                className={[
+                  styles.arrowImage,
+                  "z-50 w-28 animate-horizonbounce",
+                ].join(" ")}
+                alt=""
+              />
+            </a>
+          </div>
+
+          <div className="relative productimg mt-10 md:mt-0">
+            <img
+              src="/images/product8.png"
+              className="w-full enterimg"
+              alt=""
+            />
+            <a
+              href="https://mirror.xyz/globalmodelssyndicate.eth"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                src="/images/homepage/right_arrow.png"
+                className={[
+                  styles.arrowImage,
+                  "z-50 w-28 animate-horizonbounce",
+                ].join(" ")}
+                alt=""
+              />
+            </a>
+          </div>
+        </div>
+
+        <Modal
+          open={comingModalOpen}
+          handleClose={() => setComingModalOpen(false)}
+        >
+          <p className="text-gray-50 font-normal text-base font-inter text-center">
+            Coming Soon!
+          </p>
         </Modal>
 
-        <Modal open={switchModalOpen} handleClose={() => setSwitchModalOpen(false)}>
+        <Modal
+          open={switchModalOpen}
+          handleClose={() => setSwitchModalOpen(false)}
+        >
           <p className="text-gray-50 font-normal text-base font-inter text-center">
             Please switch network to Matic on metamask
           </p>
@@ -168,37 +259,41 @@ function Home(props) {
               We are currently in BETA and whitelisting designers!
             </p>
             <p className="text-gray-50 font-normal text-base font-inter text-center mt-6">
-              If you would like to join the Global Designer Network and contribute to our on-chain
-              open source libraries through Fractional Garment Ownership then please join our
-              discord or telegram and reach out!
+              If you would like to join the Global Designer Network and
+              contribute to our on-chain open source libraries through
+              Fractional Garment Ownership then please join our discord or
+              telegram and reach out!
             </p>
             <p className="text-gray-50 font-extrabold text-base font-inter text-center mt-6">
-              Join us on our mission as we storm the gates of the metaverse and enable the
-              gatemakers for web3 fashion and beyond!
+              Join us on our mission as we storm the gates of the metaverse and
+              enable the gatemakers for web3 fashion and beyond!
             </p>
           </p>
         </Modal>
 
         <Modal open={open1} handleClose={handleClose1}>
           <p className="text-gray-50 font-normal text-base font-inter text-center">
-            <p className="text-gray-50 font-normal text-base font-inter text-center">Hey!</p>
+            <p className="text-gray-50 font-normal text-base font-inter text-center">
+              Hey!
+            </p>
             <p className="text-gray-50 font-extrabold text-base font-inter text-center mt-6">
               We are currently in BETA and whitelisting designers!
             </p>
             <p className="text-gray-50 font-normal text-base font-inter text-center mt-6">
-              If you would like to join the Global Designer Network and contribute to our on-chain
-              open source libraries through Fractional Garment Ownership then please join our
-              discord or telegram and reach out!
+              If you would like to join the Global Designer Network and
+              contribute to our on-chain open source libraries through
+              Fractional Garment Ownership then please join our discord or
+              telegram and reach out!
             </p>
             <p className="text-gray-50 font-extrabold text-base font-inter text-center mt-6">
-              Join us on our mission as we storm the gates of the metaverse and enable the
-              gatemakers for web3 fashion and beyond!
+              Join us on our mission as we storm the gates of the metaverse and
+              enable the gatemakers for web3 fashion and beyond!
             </p>
           </p>
         </Modal>
       </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
